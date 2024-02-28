d51a05d126a47bc067929a7a961a83253e323354
Leifer Mendez <leifer33@gmail.com>
2024-02-28 11:14:00 +0100

    docs: env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-export const MAKE_GET_PROMPTS_TEMPLATE = 'https://hook.eu2.make.com/s4vbwccdaehroz2ba4m4beo0tuhpvl7u'
+export const MAKE_GET_PROMPTS_TEMPLATE = process.env.MAKE_GET_PROMPTS_TEMPLATE ?? ''
 
-export const MAKE_ADD_TO_CALENDAR = 'https://hook.eu2.make.com/47p87mq25c0qc51gc8g5l6d9mlqo96sq'
+export const MAKE_ADD_TO_CALENDAR = process.env.MAKE_ADD_TO_CALENDAR ?? ''
 
-export const MAKE_GET_FROM_CALENDAR = 'https://hook.eu2.make.com/07e1u19yalhj7nj3bt6oojvwghkeryh1'
+export const MAKE_GET_FROM_CALENDAR = process.env.MAKE_GET_FROM_CALENDAR ?? ''
